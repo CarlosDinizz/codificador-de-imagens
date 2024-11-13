f3b6fc99e4c6a42d5a5a481f7c1ddd6e52abbce1
CarlosDinizz <carlosdzalmeida@gmail.com>
2024-11-13 20:15:51 -0300

    Leitura por argv e função de escolha

--- main.c
+++ main.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-int main(){
+void escolha(char valor[], char arquivo[]);
 
-    printf("Hello World!\n");
+#define TAMANHO 25
 
+int main(int argc, char **argv){
+
+    char argumento[TAMANHO];
+    char nomeArquivo[TAMANHO];
+    strcpy(argumento, argv[1]);
+    strcpy(nomeArquivo, argv[2]);
+    
+
+    escolha(argumento, nomeArquivo);
+    
     return 0;
+}
+
+/*
+bool todosIguais(int matriz[][], int linha, int coluna){
+    
+}
+*/
+
+void escolha(char valor[], char arquivo[]){
+
+    if (strcmp(valor, "-?") == 0 || strcmp(valor, "--help") == 0){
+        printf("Help\n");
+        //menu();
+    }
+    if (strcmp(valor, "-m") == 0|| strcmp(valor, "--manual") == 0){
+        //inserirDados();
+        printf("Inserir dados");
+    }
+    if (strcmp(valor, "-f") == 0 || strcmp(valor, "--file") == 0){
+        printf("Ler arquivo\n");
+        //lerArquivo(arquivo);
+    }
 }
